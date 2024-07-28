c6a5ecae4d29d895f49cfbbf1745eb523b6b93ad
Onasis McCuien <onasismccuien@yahoo.com>
2024-07-28 16:02:12 -0500

    Module 4 Complete Basline

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var controller = require('../controllers/travel');
+var controller = require('./controllers/travel');
 
 /* Get travel page. */
 router.get('/', controller.travel);
