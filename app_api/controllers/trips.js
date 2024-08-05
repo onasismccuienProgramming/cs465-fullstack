96abdef2ff9ada1a43acf07e1208236843e7c65a
Onasis McCuien <onasismccuien@yahoo.com>
2024-08-04 20:15:59 -0500

    Module 5 completed basline

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const Trip = require('../models/travlr'); //Register model
+const Model = mongoose.model('trips');
+
+//Get: /trips - lists all the trips
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsList = async(req, res) => {
+    const q = await Model
+    .find({}) // No filter, return all records
+    .excec();
+
+    // Uncomment the following line to show results of querey
+    // on the console
+    // console.log(q):
+
+if(!q)
+{ // Database returned no data
+    return res
+            .status(404)
+            .jason(err);
+}   else { //Return resulting trip list
+      return res
+        .status(200)
+        .json(q);
+    }
+};
+module.exports = {
+    tripsList
+};
+const tripsFindByCode = async(req, res) => {
+    const q = await Model
+        .find({'code' : req.params.tripCode}) // Return single record
+        .exec();
+
+        //Uncomment the following line to show results of querey
+        //on the console
+        //console.log(q);
+
+
+
+    if(!q)
+    {// Database returned no data
+        return res
+                .status(404)
+                .json(err);
+    } else {//Return resulting trip list
+        return res
+            .status(200)
+            .json(q);
+        
+    
+    }
+};
+
+module.exports = {
+    tripsList,
+    tripsFindDyCode
+};
